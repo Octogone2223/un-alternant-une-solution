f98598324995f8924b0de391952d35d33b220df6
Kaym Kassai <yimkaboss@gmail.com>
2022-03-11 10:50:29 +0100

    Design signup page

--- authentication/urls.py
+++ authentication/urls.py
@@ -5,9 +5,9 @@ from . import views
 from app import views as app_views
 
 urlpatterns = [
+    path('', app_views.home, name="home"),
     path('sign-up/', views.sign_up, name='sign_up'),
     path('sign-in/', views.sign_in, name='sign_in'),
     path('sign-out/', views.sign_out, name='sign_out'),
     path('private-test/', views.private, name='private'),
-    path('home/', app_views.home, name="home")
 ]
