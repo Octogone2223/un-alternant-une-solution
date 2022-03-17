2432cdf88cdfb192a5063ae9256f540e605c8663
Leveque <levequetom77@gmail.com>
2022-03-17 11:15:16 +0100

    Add Partially Traduction for SignIn and SignUp

--- authentication/urls.py
+++ authentication/urls.py
@@ -9,8 +9,8 @@ app_name = 'authentication'
 
 urlpatterns = [
     path('', app_views.home, name="home"),
-    path('sign-up/', views.sign_up, name='signup'),
-    path('sign-in/', views.sign_in, name='signin'),
-    path('sign-out/', views.sign_out, name='signout'),
+    path('sign-up/', views.sign_up, name='sign_up'),
+    path('sign-in/', views.sign_in, name='sign_in'),
+    path('sign-out/', views.sign_out, name='sign_out'),
     path('private-test/', views.private, name='private'),
 ]
