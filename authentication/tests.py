d4c439329146e0e34008092c8dd6c23fae8c3951
prynge <37028634+prynge@users.noreply.github.com>
2022-03-17 13:38:58 +0100

    Update tests.py

--- authentication/tests.py
+++ authentication/tests.py
@@ -7,17 +7,20 @@ class AuthenticationTests(TestCase):
     def test_homepage(self):
         response = self.client.get('/')
         self.assertEqual(response.status_code, 200)
+
     def test_signup_page(self):
         response = self.client.get('/sign-up/')
         self.assertEqual(response.status_code, 200)
+
     def test_signin_page(self):
         response = self.client.get('/sign-in/')
         self.assertEqual(response.status_code, 200)
+
     # def test_homepage(self):
     #     response = self.client.get('/')
     #     self.assertEqual(response.status_code, 200)
 
-# Student sign up
+    # Student sign up
     def test_student_signup(self):
-        response = self.client.post("/sign-up/", {'account_type':3, 'first_name':'TestStudent','last_name':'TESTSTUDENT','email':'student@test.com','password':'TestStudent','confirmPassword':'TestStudent'})
+        response = self.client.post("/sign-up/", {'account_type': 3, 'first_name': 'TestStudent', 'last_name': 'TESTSTUDENT', 'email': 'student@test.com', 'password': 'TestStudent', 'confirmPassword': 'TestStudent'})
         self.assertEqual(response.status_code, 200)
