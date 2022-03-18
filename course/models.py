a51623301d41ca82df6551e92bfcd734424b52cd
Lilian-MMI <lilianouvmmi@gmail.com>
2022-03-18 09:14:53 +0100

    use default meta name for model class

--- course/models.py
+++ course/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Course(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    job_codes = models.ManyToManyField(
+        'job.jobCode', related_name='course_job_codes+')
+
+    create_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
