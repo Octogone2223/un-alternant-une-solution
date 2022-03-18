ae2c6b471cf7621f125ac7914f5f1aac87bd1502
Lilian-MMI <lilianouvmmi@gmail.com>
2022-03-18 10:01:20 +0100

    design about page

--- app/urls.py
+++ app/urls.py
@@ -8,4 +8,5 @@ app_name = 'core'
 urlpatterns = [
     path("", views.home, name='home'),
     path("profile", views.profile, name='profile'),
+    path("about", views.about, name='about'),
 ]
--- course/views.py
+++ course/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
+from .models import Course
 
 # Create your views here.
 
 
 def list_courses(request):
-    return render(request, 'courses.html')
+    courses = Course.objects.all()
+    return render(request, 'courses.html', {'courses': courses})
