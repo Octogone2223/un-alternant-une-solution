0431405c8fd7da9ec124728ce5862c73c1f06008
Kaym Kassai <yimkaboss@gmail.com>
2022-03-24 09:31:47 +0100

    Add user public profile page

--- app/urls.py
+++ app/urls.py
@@ -9,4 +9,9 @@ urlpatterns = [
     path("", views.home, name='home'),
     path("profile", views.profile, name='profile'),
     path("about", views.about, name='about'),
+    path("student/<int:student_id>", views.student_detail, name="student_detail"),
+    path("company/<int:company_id>", views.company_detail, name="company_detail"),
+    path("school/<int:school_id>", views.company_detail, name="school_detail"),
+
+
 ]
