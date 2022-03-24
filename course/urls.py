0a3f6a42a2f9432b2edfcdfc57f7c594e38637b2
Lilian-MMI <lilianouvmmi@gmail.com>
2022-03-24 16:55:13 +0100

    get all formations on courses page

--- course/urls.py
+++ course/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.list_courses, name='list_courses'),
+    path('preview/<int:course_id>/', views.preview_course, name='preview_course'),
     path('<int:course_id>/', views.course_detail, name='course_detail'),
-
 ]
--- course/views.py
+++ course/views.py
@@ -1,12 +1,37 @@
 from django.shortcuts import render
 from .models import Course
+from django.http.response import HttpResponse, JsonResponse
 
 # Create your views here.
 
 
 def list_courses(request):
-    courses = Course.objects.all()
+    filters = {}
+
+    location = request.GET.get('location', None)
+    if location:
+        if location.isnumeric():
+            filters['company__zip_code'] = location
+        else:
+            filters['company__city'] = location
+
+    profession = request.GET.get('profession', None)
+    if profession:
+        filters['name__icontains'] = profession
+
+    contact_type = request.GET.get('contact_type', None)
+    if contact_type:
+        filters['contract_type__icontains'] = contact_type
+
+    courses = Course.objects.filter(**filters)[:20]
     return render(request, 'list_courses.html', {'courses': courses})
+
+
+def preview_course(request, course_id):
+    course = list(Course.objects.filter(id=course_id).values('id', 'name', 'description',
+                  'school__name', 'school__street', 'school__city', 'school__zip_code'))[0]
+
+    return JsonResponse({'course': course})
 
 
 def course_detail(request, course_id):
